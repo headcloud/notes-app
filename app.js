910e98081a668aa4683585c4aefbf30e6db77452
Vladimir Molodozhan <vladimir.molodozhan@team.wrike.com>
2020-07-20 23:41:13 +0300

    try to obtain hostname from process

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+let hostname = process.env.hostname;
+console.log(`Environment hostname is ${hostname}`);
+if (hostname == null || hostname == "") {
+    hostname = '127.0.0.1';
+}
+
 let port = process.env.PORT;
 console.log(`Environment port is ${port}`);
 if (port == null || port == "") {
