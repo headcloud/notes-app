6cf726a9181858009eb4012a715337555b71f9fd
Vladimir Molodozhan <vladimir.molodozhan@team.wrike.com>
2020-07-20 23:53:11 +0300

    fix hostname setting

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const http = require('http');
 
-let hostname = process.env.hostname;
+let hostname = process.env.hostname || '0.0.0.0';
 console.log(`Environment hostname is ${hostname}`);
-if (hostname == null || hostname == "") {
-    hostname = '127.0.0.1';
-}
 
-let port = process.env.PORT;
+let port = process.env.PORT || 8000;
 console.log(`Environment port is ${port}`);
-if (port == null || port == "") {
-  port = 8000;
-}
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
